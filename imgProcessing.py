de4851343761e9eebf073bb462704cff02551383
Manyu Sharma <manyusharma96@gmail.com>
2017-04-27 19:18:45 -0400

    Completed the processing function

--- imgProcessing.py
+++ imgProcessing.py
@@ -1,9 +1,8 @@
 import numpy as np
 import cv2
 
-
-
-
+# Takes in an image, really a numpy array, and returns a 400x400
+# image, again an array, of the area centered around a detected face
 def get400(img):
 
 	# Pretrained classifiers for face detection
@@ -15,16 +14,27 @@ def get400(img):
 	# Return a list of rectangles of detected faces
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 	for (x,y,w,h) in faces:
-	    #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+	    
 	    xMid = x + w/2
 	    yMid = y + h/2
-	    cv2.circle(img,(xMid,yMid), 5, (255,0,0), -1)
-	    cv2.rectangle(img,(xMid-200,yMid-200),(xMid+200,yMid+200),(255,0,0),2)
+	    
+	    # Images are 640 x 490
+	    if (xMid + 200 > 640):
+	    	xMid = 440
+	    elif (xMid - 200 < 0):
+	    	xMid = 200
+	    if (yMid + 200 > 490):
+	    	yMid = 290
+	    elif (yMid - 200 < 0):
+	    	yMid = 200
+
+	    crop_img = gray[yMid-200:yMid+200, xMid-200:xMid+200]
 
 	# Write image, this is just for testing.
-	cv2.imwrite('testOuput.png',img)
+	return crop_img
 
-#def main():
-img = cv2.imread("test.png")
-get400(img)
-print("hello")
+# For testing
+#img = cv2.imread("test.png")
+#crop_img = get400(img)
+#cv2.imwrite('testOuput.png',crop_img)
+#print("Complete")
