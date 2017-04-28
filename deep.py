d879eb63973318ba398f9cd8e352f76f76de5aa9
Gary Qian <gqian1@jhu.edu>
2017-04-28 14:19:25 -0400

    Load training data for deep learning

--- deep.py
+++ deep.py
@@ -35,41 +35,8 @@ np.random.seed(seed)
 
 # Load training data
 print 'Loading Training Data'
-#rootdir = 'C:/Users/gary/Documents/1DOCUMENTS/Junior/ML/Facesimile'
-rootdir = './'
-X = []
-y = []
-count = 0
-for subdir, dirs, files in os.walk(rootdir + '/cohn-kanade-images'):
-	#print "3"
-	for file in files:
-		fname = path.join(subdir, file).replace('\\','/')
-		labelpath = fname[:-4] + '_emotion.txt'
-		labelpath = labelpath.replace('/cohn-kanade-images', '/Emotion')
-		#print labelpath
-		if '.png' in fname:
-			
-			label = -1
-			try:
-				f = open(labelpath, 'r')
-				line = f.read()
-				label = int(line[3]) - 1
-			except:
-				pass
-			if label >= 0:
-				img = cv2.imread(fname)
-				img = get400(img)
-				if (img is not None):
-					count += 1
-					X.append(img)
-					y.append(label)
-					cv2.imwrite(rootdir + '/dataset/img' + str(count) + '.png', img)
-					if (count % 20 == 0): print 'Processed img ' + str(count)
-data = dict()
-data['X'] = X
-data['y'] = y
-pickle.dump(data, open( "dataset400.dat", "wb" ))
-print 'Dumped ' + str(count) + ' images'
+data = pickle.load(open('dataset400.dat','rb'))
+print 'Loaded Training Data'
 
 
 '''for subj in range(0,1000):
