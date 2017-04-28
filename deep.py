c008d361d363ccb24c7704e3004600b46c444f9e
Gary Qian <gqian1@jhu.edu>
2017-04-28 15:56:23 -0400

    Deep learning compiling - not yet working - need to tune

--- deep.py
+++ deep.py
@@ -35,9 +35,15 @@ np.random.seed(seed)
 
 # Load training data
 print 'Loading Training Data'
+imgDim = 400
 data = pickle.load(open('dataset400.dat','rb'))
-print 'Loaded Training Data'
+print 'Done'
 
+
+X_train = data['X']
+y_train = data['y']
+
+num_classes = 7
 
 '''for subj in range(0,1000):
 	subjstr = str(subj)
@@ -59,16 +65,6 @@ print 'Loaded Training Data'
 X_train = np.ndarray(shape0)
 y_train = np.ndarray((imNum0))
 
-count = 0
-for x in range(0, imNum0): #for each image in trainingData/0/*
-	fname = prestring0 + str(x) + postString
-	if path.exists(fname):
-		temp = cv2.imread(fname)
-		X_train[count] = temp
-		y_train[count] = 0
-
-		count += 1
-
 for x in range(0, imNum1): #for each image in trainingData/0/*
 	fname = prestring1 + str(x) + postString
 	if path.exists(fname):
@@ -78,48 +74,81 @@ for x in range(0, imNum1): #for each image in trainingData/0/*
 
 		count += 1
 	'''
-'''
-X_train = X_train[:count]
-y_train = y_train[:count]
+print 'Preprocessing'
+#X_train = X_train[:count]
+#y_train = y_train[:count]
 # normalize inputs from 0-255 to 0.0-1.0
-X_train = X_train.astype('float32')
+X_train = np.array(X_train).astype('float32')
 X_train = X_train / 255.0
+temp = np.zeros((X_train.shape[0],imgDim,imgDim,1))
+temp[:,:,:,0] = X_train[:,:,:]
+X_train = temp
 
-
+print X_train.shape
 # one hot encode outputs
-y_train = y_train.reshape((-1, 1))
+#y_train = y_train.reshape((-1, 1))
 #y_train = np_utils.to_categorical(y_train)
-'''
+
+print 'Done'
+
 # Create the model
-
-
+print 'Constructing Model'
 '''#deeper model
 model = Sequential()
-model.add(Convolution2D(32, 3, 3, input_shape=(32, 32, 3), activation='relu', border_mode='same'))
+model.add(Convolution2D(32, 3, 3, input_shape=(32, 32, 3), activation='relu', padding='same'))
 model.add(Dropout(0.2))
-model.add(Convolution2D(32, 3, 3, activation='relu', border_mode='same'))
+model.add(Convolution2D(32, 3, 3, activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 1)))
-model.add(Convolution2D(64, 3, 3, activation='relu', border_mode='same'))
+model.add(Convolution2D(64, 3, 3, activation='relu', padding='same'))
 model.add(Dropout(0.2))
-model.add(Convolution2D(64, 3, 3, activation='relu', border_mode='same'))
+model.add(Convolution2D(64, 3, 3, activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 1)))
-model.add(Convolution2D(128, 3, 3, activation='relu', border_mode='same'))
+model.add(Convolution2D(128, 3, 3, activation='relu', padding='same'))
 model.add(Dropout(0.2))
-model.add(Convolution2D(128, 3, 3, activation='relu', border_mode='same'))
+model.add(Convolution2D(128, 3, 3, activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 1)))
 model.add(Flatten())
 model.add(Dropout(0.2))
-model.add(Dense(1024, activation='relu', W_constraint=maxnorm(3)))
+model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu', W_constraint=maxnorm(3)))
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='softmax'))'''
 
-'''
-model = load_model('model2deep.dat')
+
+#deeper model
+model = Sequential()
+model.add(Convolution2D(32, (7, 3), input_shape=(400, 400, 1), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Convolution2D(32, (7, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 1)))
+model.add(Convolution2D(64, (7, 3), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Convolution2D(64, (7, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 1)))
+model.add(Convolution2D(128, (7, 3), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Convolution2D(128, (7, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 1)))
+model.add(Convolution2D(256, (7, 3), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Convolution2D(256, (7, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 1)))
+model.add(Flatten())
+'''model.add(Dropout(0.2))
+model.add(Dense(2048, activation='relu', kernel_constraint=maxnorm(3)))'''
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+
+print 'Done'
+#model = load_model('model2deep.dat')
 
 # Compile model
-epochs = 100
+epochs = 25
 lrate = 0.01
 decay = lrate/epochs
 sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
@@ -130,12 +159,12 @@ print(model.summary())
 
 # Fit the model
 #for i in range(len(X_train)):
-model.fit(X_train, y_train, batch_size=32, nb_epoch=epochs, verbose=1, callbacks=[], validation_split=0.0, validation_data=None, shuffle=True, class_weight=None, sample_weight=None)
+model.fit(X_train[:], y_train, batch_size=5, epochs=epochs, verbose=1, callbacks=[], validation_split=0.3, shuffle=True, class_weight=None, sample_weight=None)
 #model.fit(X_train, y_train, validation_data=(X_train, y_train), nb_epoch=epochs, batch_size=32)
 model.save('model3deep.dat')
 
 print model.predict_proba(X_train[930:1000], batch_size=32, verbose=1)
-'''
+
 """
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
