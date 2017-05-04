9519a4a9fa5a558e3371996fcafdfdea2d0f1963
Manyu Sharma <manyusharma96@gmail.com>
2017-05-03 20:41:53 -0400

    Remade PCA with new dataset, still doesn't work well.

--- PCA.py
+++ PCA.py
@@ -2,11 +2,12 @@ import numpy as np
 import cPickle as pickle
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import normalize
+from load import loadData
 
 # Load in raw data
 data = pickle.load(open( "dataset400.dat", "rb" ))
-X = data['X']
-y = data['y']
+X, y, X_test, y_test, num_classes = loadData(flatten=True, normalize=False, type='int32')
+
 
 # Number of data points
 N = len(X)
@@ -23,13 +24,16 @@ print(X.shape)
 print()
 
 # Normalize the data
+print('Normalizing data...')
 X = normalize(X, axis=1, norm='l2')
 
 # Executing PCA
-pca_2 = PCA(n_components=326)
+print('Running PCA...')
+pca_2 = PCA(n_components=10)
 X_new = pca_2.fit_transform(X)
+
+print("Explained variance")
+print(pca_2.explained_variance_.cumsum())
+print()
 print("Post PCA data matrix size:")
 print(X_new.shape)
-print()
-print("Explained variance")
-print(pca_2.explained_variance_)
