c70357e73a16cb29222815bd382df09dff2df214
Manyu Sharma <manyusharma96@gmail.com>
2017-05-16 14:00:29 -0400

    Added getBox function used in facesimile.py

--- imgProcessing.py
+++ imgProcessing.py
@@ -34,6 +34,38 @@ def get400(img):
 	# Write image, this is just for testing.
 	return crop_img
 
+# Used for facesimile processing
+def getBox(img,size):
+
+	# Pretrained classifiers for face detection
+	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+	# Return a gray image of the given image
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	# Return a list of rectangles of detected faces
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	crop_img = None
+	for (x,y,w,h) in faces:
+	    
+	    xMid = x + w/2
+	    yMid = y + h/2
+	    
+	    # Images are 640 x 480
+	    if (xMid + size/2 > 640):
+	    	xMid = 640 - size/2
+	    elif (xMid - size/2 < 0):
+	    	xMid = size/2
+	    if (yMid + size/2 > 480):
+	    	yMid = 480 - size/2
+	    elif (yMid - size/2 < 0):
+	    	yMid = size/2
+
+	    crop_img = gray[yMid-size/2:yMid+size/2, xMid-size/2:xMid+size/2]
+
+	# Write image, this is just for testing.
+	return crop_img
+
 # For testing
 #img = cv2.imread("test.png")
 #crop_img = get400(img)
